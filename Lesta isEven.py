75e871a52b5c961c39d38193dfa18fa5e138b963
dae22 <83345840+dae22@users.noreply.github.com>
2024-09-23 15:44:28 +0300

    Add files via upload

---
+++ Lesta isEven.py
@@ -0,0 +1,23 @@
+def isEven(value: int) -> bool:
+    return str(bin(value))[-1] == '0'
+# Можно реализовать проверку четности числа через двоичную систему.
+# Однако это не имеет преимущества перед стандартным методом, вследствие чего не используется
+
+
+def printIsEven(number: int):
+    if isEven(number):
+        print(f'Число {number} - четное')
+    else:
+        print(f'Число {number} - нечетное')
+
+
+def testIsEven():  # Сделаем функцию для тестирования различных предзаданных данных и одно ручного ввода
+    printIsEven(2)
+    printIsEven(39)
+    printIsEven(982)
+    printIsEven(89123751)
+    print('Введите число для проверки на четность:', end='')
+    printIsEven(int(input()))
+
+
+testIsEven()
